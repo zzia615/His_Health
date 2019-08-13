356cdb8b430c2ff955fe12555cf99e6c45b8fb68
jiaolong <zzia615@163.com>
2019-08-13 22:51:45 +0800

    新增Startup

---
+++ Jaylosy.Kernel/Interface/IStartup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jaylosy.Kernel.Interface
+{
+    public delegate int RunCallback(string result);
+    public delegate int ExecuteCmdCallback(int cmd, string result);
+    public interface IStartup
+    {
+        void Run(string[] parms, RunCallback callback);
+        void ExecuteCmd(int cmd, string data);
+        void ExecuteCmd(int cmd, string data, ExecuteCmdCallback callback);
+    }
+}
